7b322e2bede08840645f61b0d04d478224ffe9b0
konkeydong <dittoes.1@gmail.com>
2021-02-25 15:24:42 -0600

    stack minimum value

--- coding_questions/data_structures/stack.lua
+++ coding_questions/data_structures/stack.lua
@@ -6,10 +6,14 @@ package.path = package.path .. ';' .. path .. '/?.lua'
 local LinkedList = LinkedList or require 'linked_list'
 
 local seq = require "pl.seq"
+local List = require "pl.List"
 -- local dbg = require 'debugger'
 
 function Stack:new(values)
-    self = setmetatable({ list = LinkedList:new() }, Stack)
+    self = setmetatable({ 
+        list = LinkedList:new(),
+        minimum = LinkedList:new()
+    }, Stack)
 
     if values ~= nil and #values > 0
     then
@@ -22,6 +26,11 @@ function Stack:new(values)
 end
 
 function Stack:push(value)
+    if self:min() == nil or self:min() >= value
+    then
+        self.minimum:insert_at_front(value)
+    end
+
     self.list:insert_at_front(value)
 end
 
@@ -33,6 +42,11 @@ function Stack:pop()
 
     local value = self.list.head.value
     self.list:remove_from_front()
+
+    if self:min() == value
+    then
+        self.minimum:remove_from_front()
+    end
 
     return value
 end
@@ -53,6 +67,20 @@ function Stack:is_empty()
     else
         return false
     end
+end
+
+function Stack:min()
+    if self:is_empty()
+    then
+        return nil
+    end
+    
+    if self.minimum.count == 0
+    then
+        self.minimum:push(2 ^ 64)
+    end
+
+    return self.minimum.head.value
 end
 
 function Stack:count()
---
+++ coding_questions/stacks_and_queues/02_stack_min.lua
@@ -0,0 +1,14 @@
+--[[
+    Stack Min: how would you design a stack which, in addition to push and pop, has a function
+        min() which returns the minimum element? Push(), pop(), and min() should all operate in O(1) time
+]]
+
+local path = require 'pl.path'.abspath('../data_structures')
+package.path = package.path .. ';' .. path .. '/?.lua'
+local Stack = Stack or require 'stack'
+
+-- Solution 1: cache the result (breaks the O(1) of push/pop)
+-- Solution 2: use a separate stack to store min values. Keep in mind that 
+--      that the min won't change that often.
+local stack = Stack:new({1, 2, 3})
+print('minimum value is: ' .. tostring((stack:min())))
