99c6bc82fc01b144cf20c1ef3b0916635ae8133b
konkeydong <dittoes.1@gmail.com>
2021-03-06 22:57:16 -0600

    basic sort

--- coding_questions/stacks_and_queues/05_sort_stack.lua
+++ coding_questions/stacks_and_queues/05_sort_stack.lua
@@ -8,3 +8,64 @@
     The stack supports the following operations:
     push(), pop(), peek(), and is_empty().
 ]]
+
+local path = require 'pl.path'.abspath('../data_structures')
+package.path = package.path .. ';' .. path .. '/?.lua'
+local Stack = Stack or require 'stack'
+
+local dbg = require 'debugger'
+
+
+function swap(stack1, stack2)
+    local temp = stack1:pop()
+    stack1:push(stack2:pop())
+    stack2:push(temp)
+end
+
+function both_not_empty(stack1, stack2)
+    if not stack1:is_empty() and not stack2:is_empty()
+    then
+        return true
+    else
+        return false
+    end
+end
+
+-- if true, stack1:peek() value is smaller than stack2:peek()
+function compare(stack1, stack2)
+    if both_not_empty(stack1, stack2)
+    then
+
+        if stack1:peek() > stack2:peek()
+        then
+            return true
+        else
+            return false
+        end
+    end
+
+    return nil
+end
+
+local main_stack = Stack:new({2, 1, 3})
+local temp_stack = Stack:new()
+
+while not main_stack:is_empty() do
+    if temp_stack:is_empty()
+    then
+        temp_stack:push(main_stack:pop())
+    end
+
+    local compare_result = compare(main_stack, temp_stack)
+    if compare_result
+    then
+        swap(main_stack, temp_stack)
+    else
+        temp_stack:push(main_stack:pop())
+    end
+end
+
+-- Basic sort, but can't handle special cases. Will need to investigate.
+temp_stack.list:print()
+dbg()
+print('TEST')
