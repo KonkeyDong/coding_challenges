164c66c5685754a9a0f02d174cb63456d01b82c6
konkeydong <dittoes.1@gmail.com>
2021-03-08 12:49:21 -0600

    rearrange folders and add chapter 4 questions

---
+++ coding_questions/04_trees_and_graphs/08_first_common_ancestor.lua
@@ -0,0 +1,8 @@
+--[[
+    First Common Ancestor
+
+    Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree.
+    Avoid storing additional nodes in a data structure.
+
+    NOTE: this is not necessarily a binary search tree.
+]]
