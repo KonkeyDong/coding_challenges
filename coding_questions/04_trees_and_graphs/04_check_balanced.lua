250aa4d46f247045425a924f3d45c3f6ce9f6530
konkeydong <dittoes.1@gmail.com>
2021-03-18 14:40:21 -0500

    check if tree is balanced

--- coding_questions/04_trees_and_graphs/04_check_balanced.lua
+++ coding_questions/04_trees_and_graphs/04_check_balanced.lua
@@ -5,3 +5,81 @@
     a balanced tree is defined to be a tree such that the heights of the two subtrees of any node
     never differ by more than one.
 ]]
+
+local BinaryTree = BinaryTree or require 'binary_tree'
+local List = require 'pl.List'
+-- local dbg = require 'debugger'
+
+-- Solution 1: recurse down and check heights on each node (slightly less efficient than solution 2, but it works)
+function check_balance(root)
+    local left = depth(root.left)
+    local right = depth(root.right)
+    local result = math.abs(left - right)
+
+    return (result <= 1)
+end
+
+function depth(node)
+    if not node
+    then
+        return 0
+    end
+
+    local left = 1 + depth(node.left)
+    local right = 1 + depth(node.right)
+    local max = math.max(left, right)
+    return max
+end
+
+function display(tree)
+    print('Is the tree balanced? ' .. tostring(check_balance2(tree.root)))
+end
+
+INT_MIN = -1 * ((2 ^ 64) - 1)
+
+-- solution 2: Check if the tree is balanced at the same time as we're checking heights.
+function check_balance2(root)
+    print()
+    return check_height(root) ~= INT_MIN
+end
+
+function check_height(root)
+    if root == nil
+    then
+        return -1
+    end
+    
+    local left_height = check_height(root.left)
+    if left_height == INT_MIN
+    then
+        return INT_MIN
+    end
+    
+    local right_height = check_height(root.right)
+    if right_height == INT_MIN
+    then
+        return INT_MIN
+    end
+    
+    local height_difference = math.abs(left_height - right_height)
+    print('left: ' .. left_height .. ' | right_height: ' .. right_height)
+    if height_difference > 1
+    then
+        return INT_MIN
+    else
+        return math.max(left_height, right_height) + 1
+    end
+end
+
+
+local tree = BinaryTree:new({1, 2, 3})
+display(tree) -- false
+
+local tree = BinaryTree:new({5, 3, 7, 2, 4, 6, 8, 1, 9})
+display(tree) -- true
+
+local tree = BinaryTree:new({5, 3, 7, 2, 4, 6, 8, 9})
+display(tree) -- true
+
+local tree = BinaryTree:new({5, 3, 8, 2, 4, 7, 9, 6, 10}) -- right side will have 1 depth longer than the left
+display(tree) -- true
