9a227d799a08419e30cede5f93b73ccc1668b823
konkeydong <dittoes.1@gmail.com>
2021-04-27 17:53:09 -0500

    prep questions

---
+++ coding_questions/05_bit_manipulations/02_binary_to_string.lua
@@ -0,0 +1,7 @@
+--[[
+    Binary to String
+
+    Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double,
+    pass the binary representation. If the number cannot be represented accurately in
+    binary with at most 32 characters, print "ERROR."
+]]
---
+++ coding_questions/05_bit_manipulations/03_flip_bit_to_win.lua
@@ -0,0 +1,11 @@
+--[[
+    Flip Bit to Win
+
+    You have an integer and you can fiip exactly one bit rom a 0 to a 1.
+    Write code to find the length of the longest sequence of 1s you could create.
+
+    Ex:
+
+    Input : 1775 (or: 11011101111)
+    Output: 8
+]]
