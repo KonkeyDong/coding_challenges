ede572e3ff39b9490c0c05e156a7edb9dfd856af
konkeydong <dittoes.1@gmail.com>
2021-05-04 13:45:59 -0500

    add power set semi solution

--- coding_questions/08_recursion_and_dynamic_programming/04_power_set.lua
+++ coding_questions/08_recursion_and_dynamic_programming/04_power_set.lua
@@ -4,29 +4,46 @@
     Write a method to return all subsets of a set.
 ]]
 
-WIP
-
-local dbg = require 'debugger'
-
-local sub_sets = {}
+-- NOTE: This kind of works, but I think Lua is showing some problems
+--       whenever it comes to arrays of arryas with no data in sub arrays.
+--       I'll have to replicate this using a different language. Probably python.
 
 function power_set(set)
-    if #set == 0 then
-        return
+    local all_subsets = {}
+    local max = 1 << #set
+
+    for k = 1, max do
+        local subset = convert_int_to_set(k, set)
+        table.insert(all_subsets, subset)
     end
 
-    table.insert(sub_sets, set)
-
-    for i, v in ipairs(set) do
-        local first = v
-        set[i] = nil
-        power_set(set)
-        table.insert(set, first)
-    end
+    return all_subsets
 end
 
-power_set({1, 2, 3, 4})
+function convert_int_to_set(x, set)
+    local subset = {}
+    local index = 0
 
-dbg()
-print("hi")
+    local k = x
+    while k > 0 do
+        if (k & 1) == 1 then
+            table.insert(subset, set[index])
+        end
 
+        index = index + 1
+
+        k = k >> 1
+    end
+
+    return subset
+end
+
+local x = power_set({1, 2, 3, 4})
+
+print("listing sets")
+for _, subset in ipairs(x) do
+    for _, v in ipairs(subset) do
+        io.write(v .. " ")
+    end
+    print()
+end
